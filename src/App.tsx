1f95d48a58c96b16039a4ad685fe749d4f09ac41
aiandyou50 <me@aiandyou.me>
2025-10-18 23:39:39 +0900

    feat: App 및 Game 컴포넌트에 TonConnect 통합 및 스핀 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 // src/App.tsx
 import React from 'react';
 import Game from './components/Game';
+import { TonConnectUIProvider, TonConnectButton } from '@tonconnect/ui-react';
+import { TON_CONNECT_MANIFEST_URL } from './constants';
 
 function App() {
-  return <Game />;
+  return (
+    <TonConnectUIProvider manifestUrl={TON_CONNECT_MANIFEST_URL}>
+      <div>
+        <header style={{ position: 'absolute', top: 12, right: 12 }}>
+          <TonConnectButton />
+        </header>
+
+        <main>
+          <Game />
+        </main>
+      </div>
+    </TonConnectUIProvider>
+  );
 }
 
 export default App;
