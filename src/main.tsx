c16a19e065c55e98d39d84326a4899aeaf22d6d5
aiandyou50 <me@aiandyou.me>
2025-11-01 22:34:12 +0900

    fix: 프론트엔드에 Buffer 폴리필 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+import { Buffer } from 'buffer';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import App from './App';
 import './styles/index.css';
+
+// Buffer 폴리필을 전역으로 설정
+(window as any).Buffer = Buffer;
 
 // TON Connect Manifest URL
 const manifestUrl = `${window.location.origin}/tonconnect-manifest.json`;
